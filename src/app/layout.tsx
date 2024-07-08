b185785aade73f2d64bad6485bb73e7df5a7583c
yuzhanzhan <yuzhanzhan@enmonster.com>
2024-07-08 16:42:33 +0800

    feat: 功能完善

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,12 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { cn } from '@/lib/utils'
+import HotHeader from "@/components/layout/header";
+import HotMain from "@/components/layout/main";
+import ThemeProvider from "@/components/context/theme";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ["latin"], variable: "--font-sans" });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -15,8 +19,24 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+    <html lang="en" suppressHydrationWarning>
+      <body className={`
+        ${cn(
+          "min-h-screen bg-background font-sans antialiased",
+          inter.variable
+          )} w-[100vw] h-[100vh]`
+        } >
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange>
+          <HotHeader></HotHeader>
+          <HotMain>
+            {children}
+          </HotMain>
+        </ThemeProvider>
+      </body>
+  </html>
   );
 }
